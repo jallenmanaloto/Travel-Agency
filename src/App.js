40c05334a942266537d3afda0e6541f6b0d5776f
jallenmanaloto <84300846+jallenmanaloto@users.noreply.github.com>
2022-02-14 21:40:49 +0800

    Created context for drawer display

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import { useState } from "react";
 import "./App.css";
+import { ActiveDrawer } from "./context/Drawer";
 import Head from "./components/landing/header/Head";
 
 function App() {
+  //setting context value
+  const [drawerActive, setDrawerActive] = useState(false);
+
   return (
-    <div className="App">
-      <Head />
-    </div>
+    <ActiveDrawer.Provider value={{ drawerActive, setDrawerActive }}>
+      <div className="App">
+        <Head />
+      </div>
+    </ActiveDrawer.Provider>
   );
 }
 
