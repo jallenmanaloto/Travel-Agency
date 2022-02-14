47cee80b06795ebf16e2a830a0748154e6c5473c
jallenmanaloto <84300846+jallenmanaloto@users.noreply.github.com>
2022-02-14 19:25:45 +0800

    Add font Inter

--- src/components/landing/header/Nav.js
+++ src/components/landing/header/Nav.js
@@ -1,7 +1,7 @@
 import Button from "@mui/material/Button";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
-import { fontPoppins } from "../../../themes/Font";
+import { fontPoppins, fontInter } from "../../../themes/Font";
 import { ThemeProvider } from "@mui/material/styles";
 
 const Nav = () => {
@@ -71,22 +71,27 @@ const Nav = () => {
         </Grid>
         <Grid item xs={6} sm={2} md={3} lg={3}>
           <div className="account">
-            <Button
-              variant="outlined"
-              sx={{
-                border: "2px solid rgba(119, 127, 150, 1)",
-                color: "white",
-                height: "100%",
-                fontWeight: "bold",
-                "&:hover": {
-                  backgroundColor: "white",
-                  borderColor: "white",
-                  color: "#777F96",
-                },
-              }}
-            >
-              Create Account
-            </Button>
+            <ThemeProvider theme={fontInter}>
+              <Button
+                variant="outlined"
+                sx={{
+                  border: "2px solid rgba(119, 127, 150, 1)",
+                  color: "white",
+                  height: "100%",
+                  width: "152px",
+                  fontWeight: "bold",
+                  fontSize: "14px",
+                  textTransform: "none",
+                  "&:hover": {
+                    backgroundColor: "white",
+                    borderColor: "white",
+                    color: "#777F96",
+                  },
+                }}
+              >
+                Create Account
+              </Button>
+            </ThemeProvider>
           </div>
         </Grid>
       </Grid>
