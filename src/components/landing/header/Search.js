ea86527601b3deaea20697b94eb4cf1aee54ce08
jallenmanaloto <84300846+jallenmanaloto@users.noreply.github.com>
2022-02-14 20:01:40 +0800

    Add Grid layout to Form

--- src/components/landing/header/Search.js
+++ src/components/landing/header/Search.js
@@ -2,9 +2,8 @@ import React, { useState } from "react";
 import { styled } from "@mui/material/styles";
 import Button from "@mui/material/Button";
 import Divider from "@mui/material/Divider";
-import FormControl from "@mui/material/FormControl";
+import Grid from "@mui/material/Grid";
 import InputBase from "@mui/material/InputBase";
-import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
 import Select from "@mui/material/Select";
 import FmdGoodOutlinedIcon from "@mui/icons-material/FmdGoodOutlined";
@@ -37,58 +36,65 @@ const Search = () => {
     <div className="form-wrapper">
       <ThemeProvider theme={fontInter}>
         <div className="form-container">
-          <div className="field-container">
-            <SearchRoundedIcon
-              sx={{ ml: "34px", color: "#AFAEB4", fontSize: "1.8rem" }}
-            />
-            <Divider
-              orientation="vertical"
-              sx={{ ml: "0.5rem", height: "1.2rem", color: "#E5E5E5" }}
-            />
-            <UnstyledInput
-              placeholder="Search destination"
-              id="destination-search"
-              sx={{ ml: "0.2rem" }}
-            />
-            <Divider
-              orientation="vertical"
-              sx={{ height: "27px", color: "#E5E5E5" }}
-            />
-            <FmdGoodOutlinedIcon
-              sx={{ ml: "27px", color: "#AFAEB4", fontSize: "1.8rem" }}
-            />
-            <UnstyledInput
-              placeholder="C. Location"
-              id="location"
-              sx={{ ml: "0.8rem", width: "18%" }}
-            />
-            <Divider
-              orientation="vertical"
-              sx={{ height: "27px", color: "#E5E5E5" }}
-            />
-            <Select
-              value={guest}
-              input={<UnstyledInput />}
-              sx={{ width: "20%", ml: "27px" }}
-              label="2 Person"
-              onChange={handleGuestInput}
-            >
-              <MenuItem value="1 Person">1 Person</MenuItem>
-              <MenuItem value="2 Person">2 Person</MenuItem>
-              <MenuItem value="3 Person">3 Person</MenuItem>
-            </Select>
-          </div>
-          <Button
-            sx={{
-              width: "152px",
-              backgroundColor: "#34A5E4",
-              fontSize: "1.1rem",
-              textTransform: "none",
-            }}
-            variant="contained"
-          >
-            Search
-          </Button>
+          <Grid container spacing={3}>
+            <Grid item lg={10}>
+              <div className="field-container">
+                <SearchRoundedIcon
+                  sx={{ ml: "34px", color: "#AFAEB4", fontSize: "1.8rem" }}
+                />
+                <Divider
+                  orientation="vertical"
+                  sx={{ ml: "0.5rem", height: "1.2rem", color: "#E5E5E5" }}
+                />
+                <UnstyledInput
+                  placeholder="Search destination"
+                  id="destination-search"
+                  sx={{ ml: "0.2rem" }}
+                />
+                <Divider
+                  orientation="vertical"
+                  sx={{ height: "27px", color: "#E5E5E5" }}
+                />
+                <FmdGoodOutlinedIcon
+                  sx={{ ml: "27px", color: "#AFAEB4", fontSize: "1.8rem" }}
+                />
+                <UnstyledInput
+                  placeholder="C. Location"
+                  id="location"
+                  sx={{ ml: "0.8rem", width: "18%" }}
+                />
+                <Divider
+                  orientation="vertical"
+                  sx={{ height: "27px", color: "#E5E5E5" }}
+                />
+                <Select
+                  value={guest}
+                  input={<UnstyledInput />}
+                  sx={{ width: "20%", ml: "27px" }}
+                  label="2 Person"
+                  onChange={handleGuestInput}
+                >
+                  <MenuItem value="1 Person">1 Person</MenuItem>
+                  <MenuItem value="2 Person">2 Person</MenuItem>
+                  <MenuItem value="3 Person">3 Person</MenuItem>
+                </Select>
+              </div>
+            </Grid>
+            <Grid item lg={2}>
+              <Button
+                sx={{
+                  width: "152px",
+                  height: "100%",
+                  backgroundColor: "#34A5E4",
+                  fontSize: "1.1rem",
+                  textTransform: "none",
+                }}
+                variant="contained"
+              >
+                Search
+              </Button>
+            </Grid>
+          </Grid>
         </div>
       </ThemeProvider>
     </div>
