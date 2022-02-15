623e8c3a3f8724070e782cac8626a3b71a1c1de7
jallenmanaloto <84300846+jallenmanaloto@users.noreply.github.com>
2022-02-15 13:41:36 +0800

    Add Info component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import "./App.css";
 import { ActiveDrawer } from "./context/Drawer";
 import Head from "./components/landing/header/Head";
+import Info from "./components/landing/info/Info";
 
 function App() {
   //setting context value
@@ -11,6 +12,7 @@ function App() {
     <ActiveDrawer.Provider value={{ drawerActive, setDrawerActive }}>
       <div className="App">
         <Head />
+        <Info />
       </div>
     </ActiveDrawer.Provider>
   );
