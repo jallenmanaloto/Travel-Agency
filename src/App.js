6e791d8e965755744c6f75580e5905859351fb33
jallenmanaloto <84300846+jallenmanaloto@users.noreply.github.com>
2022-02-15 14:26:22 +0800

    Add Explore component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "./App.css";
 import { ActiveDrawer } from "./context/Drawer";
+import Explore from "./components/landing/explore/Explore";
 import Head from "./components/landing/header/Head";
 import Info from "./components/landing/info/Info";
 
@@ -13,6 +14,7 @@ function App() {
       <div className="App">
         <Head />
         <Info />
+        <Explore />
       </div>
     </ActiveDrawer.Provider>
   );
